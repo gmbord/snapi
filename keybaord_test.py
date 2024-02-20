67a8a8ec52c8aaed9dade2ffb35448a9bbd89abb
gmbord <bordelon@mit.edu>
2024-02-20 17:37:17 -0500

    l

--- keybaord_test.py
+++ keybaord_test.py
@@ -1,47 +1,187 @@
 import keyboard
-import time
 
-team1 = False
+player1 = False
+player2 = False
+player3 = False
+player4 = False
 
-
+#player1 team 1
 def qfunc():
-    global team1
-    global team2
+    global player1
+    global player2
+    global player3
+    global player4
     
     print("tossingq")
-    team1 = True
-    print(team1)
+    player1 = True
+    player2 = False
+    player3 = False
+    player4 = False
 
-def afunc():
-    global team1
-    global team2
+
+def wfunc():
+
+    print("catchw")
+ 
+
+def efunc():
+    global player3
+    global player4
     
-    print("tossinga")
-    team1 = False
-    print(team1)
+    print("drope")
+    if player3:
+        #player3 last tossed
+        pass
+    elif player4:
+        #player4 last tossed
+        pass
+    else:
+        pass
+
+
+#player2 team 1
+def zfunc():
+    global player1
+    global player2
+    global player3
+    global player4
     
+    print("tossingz")
+    player1 = False
+    player2 = True
+    player3 = False
+    player4 = False
+
+
+def xfunc():
+
+    print("catchx")
+ 
+
+def cfunc():
+    global player3
+    global player4
     
-
-# def wfunc():
-#     print("w press")
-
-# def efunc():
-#     print("e press")
-
-# def qwefunc():
-#     print("point Q!")
-
-# def asdfunc():
-#     print("point A!")
+    print("dropc")
+    if player3:
+        #player3 last tossed
+        pass
+    elif player4:
+        #player4 last tossed
+        pass
+    else:
+        pass
 
 
+#player3 team 2
+def ifunc():
+    global player1
+    global player2
+    global player3
+    global player4
+    
+    print("tossingi")
+    player1 = False
+    player2 = False
+    player3 = True
+    player4 = False
+
+
+def ofunc():
+
+    print("catcho")
+ 
+
+def pfunc():
+    global player1
+    global player2
+    
+    print("dropp")
+    if player1:
+        #player1 last tossed
+        pass
+    elif player2:
+        #player2 last tossed
+        pass
+    else:
+        pass
+
+
+#player4 team 2
+def bfunc():
+    global player1
+    global player2
+    global player3
+    global player4
+    
+    print("tossingb")
+    player1 = False
+    player2 = False
+    player3 = False
+    player4 = True
+
+
+def nfunc():
+
+    print("catchn")
+ 
+
+def mfunc():
+    global player1
+    global player2
+    
+    print("dropm")
+    if player1:
+        #player1 last tossed
+        pass
+    elif player2:
+        #player2 last tossed
+        pass
+    else:
+        pass
+
+
+def qwefunc():
+    global player1
+    global player2
+    global player3
+    global player4
+
+    if player1:
+        #player1point
+        pass
+    elif player2:
+        #player2point
+        pass
+    elif player3:
+        #player3point
+        pass
+    elif player4:
+        #player4point
+        pass
+    else:
+        pass
+
+
+#player1 team 1
 keyboard.add_hotkey('q', qfunc)
-keyboard.add_hotkey('a', afunc)
-# keyboard.add_hotkey('e', efunc)
+keyboard.add_hotkey('w', wfunc)
+keyboard.add_hotkey('e', efunc)
+#player2 team 1
+keyboard.add_hotkey('z', zfunc)
+keyboard.add_hotkey('x', xfunc)
+keyboard.add_hotkey('c', cfunc)
 
-    
+
+#player3 team 2
+keyboard.add_hotkey('i', ifunc)
+keyboard.add_hotkey('o', ofunc)
+keyboard.add_hotkey('p', pfunc)
+#player4 team 2
+keyboard.add_hotkey('b', bfunc)
+keyboard.add_hotkey('n', nfunc)
+keyboard.add_hotkey('m', mfunc)
 
 
 
-# main()
 keyboard.wait()
