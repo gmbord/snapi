4634dddb2d3773c498d0ed766b3192bd21535639
gmbord <bordelon@mit.edu>
2024-02-20 16:29:02 -0500

    h

--- keybaord_test.py
+++ keybaord_test.py
@@ -3,7 +3,14 @@ import keyboard
 def qfunc():
     print("q")
 
+def wfunc():
+    print("w press")
+
+def efunc():
+    print("e press")
 keyboard.add_hotkey('q', qfunc)
+keyboard.add_hotkey('w', wfunc)
+keyboard.add_hotkey('e', efunc)
 
 keyboard.wait("esc")
 print("end")
